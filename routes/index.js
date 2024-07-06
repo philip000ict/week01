21106472c7ac26effa81f4fd2626edd115bdfcdf
Harry <cv3tech@gmail.com>
2024-07-07 07:42:20 +1000

    added microservice card to UI

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,16 @@ const itemlist = [
   { 'id':3, 'title': 'SIT722', 'description': 'This best unit ever'},
 ];
 
+const panellist = [
+  { 'id':1, 'title': 'Microservice', 'description': 'This is the UI for Microserver01'},
+  { 'id':2, 'title': 'Function', 'description': 'Separation of the Monolith'},
+  { 'id':3, 'title': 'Status', 'description': 'Preliminary Mockup'},
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', subheading: 'Microservice to San Fernando', items: itemlist,
-        url: 'href=https://week01-g8b3.onrender.com/', link: 'Visit Week01!'
+  res.render('index', { student: 'pbarnes',title: 'SIT722 Express', subheading: 'Microservice, trains to San Fernando', items: itemlist,
+        panel01: panellist, url: 'href=https://week01-g8b3.onrender.com/', link: 'Visit Week01!'
         });
   
 });
